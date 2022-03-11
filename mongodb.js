8f651ad14f38fa46468bbbfbda54a8e1024a69d0
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-11 11:55:41 -0800

    update data base

--- mongodb.js
+++ mongodb.js
@@ -34,20 +34,58 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(count)
     // })
 
-    db.collection('tasks').findOne({ _id: new ObjectId('622b8a6c14918548a099329a')}, (error, user) => {
-        if (error) {
-            return console.log(error)
-        }
+    // db.collection('tasks').findOne({ _id: new ObjectId('622b8a6c14918548a099329a')}, (error, user) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
 
-        console.log(user)
-    })
+    //     console.log(user)
+    // })
 
-    db.collection('tasks').find({ completed: false }).count((error, count) => {
-        console.log(count)
-    })
+    // db.collection('tasks').find({ completed: false }).count((error, count) => {
+    //     console.log(count)
+    // })
     
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
-    })
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     console.log(tasks)
+    // })
+
+
+    // db.collection('users').updateOne({ 
+    //     _id: new ObjectId('622b86c886eb64463abc6e2a')
+    // }, { 
+    //     $set: { 
+    //         name: 'Willow'
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // db.collection('users').updateOne({ 
+    //     _id: new ObjectId('622b86c886eb64463abc6e2a')
+    // }, {
+    //     $inc: { 
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('tasks').updateMany({ 
+        completed: false
+     }, { 
+         $set: { 
+             completed: true
+         }
+     }).then((result) => {
+         console.log(result.modifiedCount)
+     }).catch((error) => {
+         console.log(error)
+     })
+
 })
 
