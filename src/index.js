547455005d901737695eb1a95858b247809cc186
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-15 10:49:28 -0700

    Resource reading endpoints for tasks

--- src/index.js
+++ src/index.js
@@ -30,7 +30,18 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const _id = req.params.id
 
-    
+    User.findById(_id).then((user) => {
+        if (!user) {
+            // console.log('should send 404')
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    }).catch((error) => {
+        // console.log('should send 505')
+        res.status(500).send()
+    })
 })
 
 app.post('/tasks', (req, res) => {
@@ -40,6 +51,30 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+
+    Task.find({}).then((tasks) => {
+        res.status(200).send(tasks)
+    }).catch((erorr) => {
+        res.status(500).send()
+    })
+
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            res.status(4004).send()
+        }
+        res.send(task)
+
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
