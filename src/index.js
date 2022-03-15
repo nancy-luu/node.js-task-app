c61e6022d0c5f96a7b13a84b047735ecf854b67b
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-14 20:41:34 -0700

    Resource reading endpoints

--- src/index.js
+++ src/index.js
@@ -18,6 +18,21 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    // empty object => all users in database
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
