34c0faf9251755834e6728c4067efb26cbcbb2a1
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-15 15:53:21 -0700

    promise chaining query

---
+++ playground/promise-chaining.js
@@ -0,0 +1,12 @@
+require('../src/db/mongoose')
+const User = require('../src/models/user')
+
+
+User.findByIdAndUpdate('622fb6b1f1784f29fc4ed7db', { age: 1 }).then((user) => {
+    console.log(user)
+    return User.countDocuments({ age: 1 })
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
