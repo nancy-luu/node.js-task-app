4d4aa3c232e4ece9c4f41b4244ffe1196cdf4f3a
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-30 21:09:50 -0700

    bcrypt example 2

--- src/index.js
+++ src/index.js
@@ -19,13 +19,19 @@ app.listen(port, () => {
 
 const bcrypt = require('bcryptjs')
 
-
 const myFunction = async () => {
     const password = 'Red12345!'
     const hashedPassword = await bcrypt.hash(password, 8)
     
     console.log(password)
     console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+
+    // will result in false
+    const isMatch2 = await bcrypt.compare('nope', hashedPassword)
+    console.log(isMatch2)
 }
 
 myFunction()
