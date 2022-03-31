50b4dbfc6fb53b2065d196f7202831e0238263c3
nluuarch@gmail.com <nluuarch@gmail.com>
2022-03-30 19:08:35 -0700

    Delete enpoint for task

--- src/index.js
+++ src/index.js
@@ -154,6 +154,22 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if (!task) {
+            res.status(400).send()
+        }
+        
+        res.send(task)
+
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
