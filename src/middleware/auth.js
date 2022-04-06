489086f50fbd60488aadfbe885b9a9bd015fe8ac
nluuarch@gmail.com <nluuarch@gmail.com>
2022-04-06 15:17:28 -0700

    route handler for profile

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,17 +4,19 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorization').replace('Bearer', '')
+        const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'thisismynewcourse')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token' : token })
 
         if (!user) {
+            // triggers catch down below
             throw new Error()
         }
 
+        // route handlers will be able to access user variable from above
         req.user = user
         next()
-
+ 
     } catch (e) {
         res.status(401).send({ error: 'Please authenticate.' })
     }
