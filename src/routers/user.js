ef9a448103b136905e0e5bfbf4d0d722245b3a56
nluuarch@gmail.com <nluuarch@gmail.com>
2022-04-07 11:20:16 -0700

    All user routes have auth and profile can only edit their own

--- src/routers/user.js
+++ src/routers/user.js
@@ -71,22 +71,24 @@ router.get('/users', auth, async (req, res) => {
     }
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
 
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+//     try {
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
 
-router.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id
+
+router.patch('/users/me', auth, async (req, res) => {
+    const _id = req.user._id
+
     // .keys will return an array of strings where each is a property on that object
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -97,40 +99,43 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(_id)
+        // const user = await User.findById(_id)
 
         // make sure our middleware runs correctly
         updates.forEach((update) => {
             // bracket notation to access property dynamically instead of dot notation
-            user[update] = req.body[update]
+            req.user[update] = req.body[update]
         })
-        await user.save()
+        await req.user.save()
 
         // new: true returns the new user as opposed to the existing one before the update (gives us latest data)
         // const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
         
-        if (!user) {
-            return res.status(404).send()
-        }
+        // if (!req.user) {
+        //     return res.status(404).send()
+        // }
 
-        res.send(user)
+        res.send(req.user)
 
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id
+router.delete('/users/me', auth, async (req, res) => {
+    // if we werent using auth as middlewware then we would not  have access to req.user
+    const _id = req.user._id
 
     try {
-        const user = await User.findByIdAndDelete(_id)
+        // const user = await User.findByIdAndDelete(_id)
 
-        if (!user) {
-            return res.status(400).send()
-        }
+        // if (!user) {
+        //     return res.status(400).send()
+        // }
 
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
+
     } catch (e) {
         res.status(500).send()
     }
