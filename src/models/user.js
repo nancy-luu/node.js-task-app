9dcf52f540674c2d179815437d400138ba79a9bf
nluuarch@gmail.com <nluuarch@gmail.com>
2022-04-08 13:41:40 -0700

    delete user when user is removed

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator')
 const chalk = require('chalk')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const errorHighLight = chalk.bold.red.inverse
 
@@ -118,6 +119,14 @@ userSchema.pre('save', async function (next) {
     
     next()
 })
+
+//  delete user task when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this   
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
 
 // defining model
 const User = mongoose.model('User', userSchema)
