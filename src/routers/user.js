dddc06fc31aa6900c9481050a23b968275e729d3
nluuarch@gmail.com <nluuarch@gmail.com>
2022-04-18 19:14:57 -0700

    added field to user model to store image data

--- src/routers/user.js
+++ src/routers/user.js
@@ -163,7 +163,7 @@ const avatar = multer({
     }
 
 })
-router.post('/users/me/avatar', avatar.single('avatar'), async (req, res) => {
+router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) => {
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
